7725c92fbf5752a08976e937c2e20c941411f2c0
Venilton FalvoJr <venilton.falvo@gmail.com>
2016-03-14 23:47:36 -0300

    Exercicios 07/03/2016

--- FE2: Lição 1, Lição 2 e Lição 3/exercicio09.ino
+++ FE2: Lição 1, Lição 2 e Lição 3/exercicio09.ino
@@ -1,5 +1,5 @@
 /**
- * CÓDIGO PARA ARDUINO (extensão .ino)
+ * CÓDIGO PARA ARDUINO (extensão .ino) 
  * 
  * 9. Faça um programa que leia o valor de um sensor de temperatura TM35, que
  * varia de 0 a 1023, onde 0 corresponde a 0 Volts e 1023 corresponde a 5 Volts.
