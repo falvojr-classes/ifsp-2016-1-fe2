e8f63b0e611cb97c218950459bbc762e9c99c479
Venilton FalvoJr <falvojr@gmail.com>
2016-10-03 20:26:36 -0300

    Atualização 03/10/2016

---
+++ FE2:%20Licao%205/exercicio03.c
@@ -0,0 +1,24 @@
+/**
+ * 3. Implemente um programa que calcule o produto de 2 número
+ * inteiros X e Y. Além disso, seu algoritmo OBRIGATORIAMENTE não
+ * deve utilizar operador de multiplicação (*).
+ */
+
+#include<stdio.h>
+
+int main()
+{
+    int x, y, i, resultado = 0;
+
+    printf("Digite X e Y: ");
+    scanf("%d %d", &x, &y);
+
+    for(i = 0; i < x; i++) {
+        resultado += y;
+    }
+
+    printf("%d x %d = %d", x, y, resultado);
+
+    getchar();
+    return 0;
+}
